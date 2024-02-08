147d39a89cd4c58134cebb89935c7c9f2ca45d5d
Ruslan Voloshin <rus-voloshin_07@mail.ru>
2024-02-08 14:46:17 +0300

    save-zone-3 add pug

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,20 +1,25 @@
 import fileinclude from "gulp-file-include";
 import webpHtmlNosvg from "gulp-webp-html-nosvg";
 import versionNumber from "gulp-version-number"
+import pug from "gulp-pug";
 
 export const html = () => {
     return app.gulp.src(app.path.src.html) // получаем доступ к файлам и папкам по указанному пути
         .pipe(app.plugins.plumber(
             app.plugins.notify.onError({
                 title: "HTML",
-                message: "Error: <%= error.message%>"
+                message: "Error: <%= error.message %>"
             }))
         )
-        .pipe(fileinclude())
+        //.pipe(fileinclude())// // при установке pug можно убрать данную строку
+        .pipe(pug({
+            pretty: true, // сжатие html файла
+            verbose: true
+        }))
         .pipe(app.plugins.replace(/@img\//g, 'img/'))
         .pipe(webpHtmlNosvg())
-        .pipe (
-            versionNumber ({
+        .pipe(
+            versionNumber({
                 'value': '%DT%',
                 'append': {
                     'key': '_v',
