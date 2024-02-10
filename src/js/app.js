acba726546127ff98cd92ca9e05e92d5e3a54c11
Ruslan <rus-voloshin_07@mail.ru>
2024-02-10 17:03:24 +0300

    save-zone 5.1 working slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,21 @@
 // Сюда загружаем скрипты (к примеру slick)
 // Разрешает работу с модулями (точнее их подкачку из node_modules, а также позволяет загружать скрипты js)
 import * as flsFunctions from "./modules/functions.js";
+import jquery from "jquery";
+
+// Загрузка сладера (Siper), но в данной работе он не используется
 
 flsFunctions.isWebp();
-
 import Swiper, { Navigation, Pagination } from 'swiper';
-
 const swiper = new Swiper();
 
-import jquery from "jquery";
+// Настройка слайдеров
+
 import "slick-carousel";
 
 jquery(".carousel").slick({
     dots: true,
-    slidesToShow: 1,
+    slidesToShow: 1, // сколько слайдов на экране (по умолчанию 1, но можно и поставить 2, 3...)
+    infinite: true, // значение бесконечности (после конечного слайда будет переноситься в начало)
+
 });
