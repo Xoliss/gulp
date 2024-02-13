9f2924bf69d4dc01e7836d682f641d342bac8658
Ruslan Voloshin <rus-voloshin_07@mail.ru>
2024-02-13 11:16:51 +0300

    New-start

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -15,7 +15,7 @@ export const scss = () => {
                 title: "SCSS",
                 message: "Error: <% =error.message %>"
             })))
-        .pipe(app.plugins.replace(/@img\//g, '../img/'))
+        .pipe(app.plugins.replace(/@img\//g, '../../img/')) // изменяем путь до картинкок (удобнее будет)
         .pipe(sass({
             outputStyle: 'expanded'
         }))
