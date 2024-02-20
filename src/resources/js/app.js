781df44d70af27b7b4ff4aa09742798b7196c4ab
Ruslan Voloshin <rus-voloshin_07@mail.ru>
2024-02-20 18:01:01 +0300

    workinggit add .! slider, but need to do open slider (he is in comment)

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -4,51 +4,66 @@
 // flsFunctions.name_function(); // прописываем все функции из functions.js
 
 import Swiper from "swiper";
-import { Controller, Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Controller, Pagination } from 'swiper/modules'
 
 
-let swiperStandart = new Swiper('.swiper--standart', {
-    modules: [Navigation, Pagination],
+let swiperStandart = new Swiper('.swiper--standart');
 
-    loop: true,
-    slidesPerView: 1,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-
-    // Navigation arrows
-    navigation: { 
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
+let swiperLarge = new Swiper('.swiper--large');
 
 let swiperMini = new Swiper('.swiper--mini', {
-    modules: [Navigation, Pagination],
+
+    modules: [Navigation, Controller],
+
+    loop: true,
+    slidesPerView: 3,
+    nested: true,
+    slidesPerGroup: 1,
+
+    controller: {
+        control: swiperStandart
+    },
+
+});
+
+new Swiper('.swiper--standart', {
+    modules: [Navigation, Controller],
 
     loop: true,
     slidesPerView: 1,
-    nested: true,
+    slidesPerGroup: 1,
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-
-    // Navigation arrows
-    navigation: {
+    navigation: { 
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
 
     controller: {
-        control: swiperStandart
+        control: swiperMini
     },
 });
 
-swiperMini.controller.control = swiperStandart;
-swiperStandart.controller.control = swiperMini;
+new Swiper('.swiper--large', {
+    modules: [Navigation, Pagination, Controller],
+
+    loop: true,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+
+    navigation: { 
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+    },      
+
+    controller: {
+        control: swiperMini
+    },
+});
+
+
+// swiperMini.controller.control = swiperStandart;
+// swiperStandart.controller.control = swiperMini;
