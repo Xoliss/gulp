c3fa863391751a99b20603ed6690ad58ca8038fb
Ruslan Voloshin <rus-voloshin_07@mail.ru>
2024-02-21 14:10:50 +0300

    save-zone 11

--- src/resources/js/modules/functions.js
+++ src/resources/js/modules/functions.js
@@ -1,35 +1,65 @@
 // Здесь описываем все функции
 // функция проверки (Поддерживается ли браузером картинки WEBP?)
 export function isWebp() {
-    function testWebp (callback) {
-        let webP = new Image();
-        webP.onload = webP.onerror = function () {
-            callback(webP.height == 2);
-        };
-        webP.src = ""
-    }
-    testWebp(function(support) {
-        let className = support == true ? 'webp' : 'no-webp'
-        document.documentElement.classList.add(className);
-    });
+  function testWebp(callback) {
+    let webP = new Image();
+    webP.onload = webP.onerror = function () {
+      callback(webP.height == 2);
+    };
+    webP.src = ""
+  }
+  testWebp(function (support) {
+    let className = support == true ? 'webp' : 'no-webp'
+    document.documentElement.classList.add(className);
+  });
 }
 
-export function openSlider() {
-    const slider = document.querySelector('.swiper--large');
-    const overlay = document.createElement('div');
-    overlay.classList.add('overlay');
-    const images = document.querySelectorAll('.swiper--standart__img');
+// открытие и закрытие большого слайдера
 
-    images.forEach(image => {
-      image.addEventListener('click', () => {
-        slider.style.display = 'block'; // Показываем слайдер
-        document.body.appendChild(overlay); // Добавляем overlay в body
-      });
+export function openSlider() {
+  const slider = document.querySelector('.swiper--large');
+  const overlay = document.createElement('div');
+  overlay.classList.add('overlay');
+  const images = document.querySelectorAll('.swiper--standart__img');
+
+  images.forEach(image => {
+    image.addEventListener('click', () => {
+      slider.style.display = 'block'; // Показываем слайдер
+      document.body.appendChild(overlay); // Добавляем overlay в body
     });
+  });
+
+  overlay.addEventListener('click', () => {
+    slider.style.display = 'none'; // Скрываем слайдер
+    slider.style.position = 'static'; // Устанавливаем позицию "static"
+    document.body.removeChild(overlay); // Удаляем overlay из body
+  });
+}
+
+// открытие и скрытие меню консультации
+
+export function showConsultation() {
+
+  var consultationContent = document.querySelector('.consultation');
+  var overlay = document.querySelector('.overlay');
+
+  consultationContent.style.display = 'block';
   
-    overlay.addEventListener('click', () => {
-      slider.style.display = 'none'; // Скрываем слайдер
-      slider.style.position = 'static'; // Устанавливаем позицию "static"
-      document.body.removeChild(overlay); // Удаляем overlay из body
-    });
-  }
+  overlay.style.display = 'block';
+}
+
+export function hideConsultation() {
+  var consultationContent = document.querySelector('.consultation');
+  var overlay = document.querySelector('.overlay');
+
+  consultationContent.style.display = 'none';
+  overlay.style.display = 'none';
+}
+
+var buttons = document.querySelectorAll('.button');
+buttons.forEach(function (button) {
+  button.addEventListener('click', showConsultation);
+});
+
+var closeButton = document.querySelector('.consultation__close');
+closeButton.addEventListener('click', hideConsultation);
